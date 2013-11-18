b9ef3e0ed1965fa3860355c90671b67d6d46221c
nutannivate <nutannivate@gmail.com>
2013-11-18 10:30:07 +0530

    Subject: pagination using default bootstrap theme.
    Description:

--- static/js/paging.js
+++ static/js/paging.js
@@ -1,18 +1,18 @@
-/*
-js module for paging
-*/
+//js module for pagination
+
+var context;
+var old_page = 1;
 
 function Pager(tableName, itemsPerPage) {
-    this.count = 0;
+    context = this;
     this.tableName = tableName;
     this.itemsPerPage = itemsPerPage;
-    this.currentPage = 1;
+    currentPage = 1;
     this.pages = 0;
-    this.inited = false;
     this.records;
     
     /*
-    show list items in a table row
+    display specific records in a table row 
     */
     this.showRecords = function(from, to) {        
         var rows = document.getElementById(tableName).rows;
@@ -26,108 +26,76 @@ function Pager(tableName, itemsPerPage) {
     }
     
     /*
-    find out the range of items to be displayed for particular page number
-    and call a css class for selected page number from navigator
+    set style for old page anchor and new page anchor
+    and get the range of list items to be shown for selected page
     */
     this.showPage = function(pageNumber) {
-    	
-    	if (! this.inited) {
-    		alert("not inited");
-    		return;
-    	}
-    	
-        var oldPageAnchor = document.getElementById('pg'+pageNumber);
-        oldPageAnchor.className = 'pg-normal';
+        var oldPageAnchor = document.getElementById('pg'+old_page);
+        oldPageAnchor.style.textDecoration = 'none';
+        oldPageAnchor.style.fontWeight = "normal";
         
-        this.currentPage = pageNumber;
-        var newPageAnchor = document.getElementById('pg'+this.currentPage);
-        newPageAnchor.className = 'pg-selected';
+        currentPage = pageNumber;
+        var newPageAnchor = document.getElementById('pg'+currentPage);
+        newPageAnchor.style.textDecoration = 'underline';
+        newPageAnchor.style.fontWeight = "bold";
         
         var from = (pageNumber - 1) * itemsPerPage;
         var to = from + itemsPerPage - 1;
         this.showRecords(from, to);
-    }   
+        old_page = currentPage;
+    }       
     
     /*
-    previous button to display the prev 3 pages and reset the navigator accordigly
-    */
-    this.prev = function() {
-        this.count = this.count - 3;
-        if(this.count >= 0){
-		if (this.currentPage < this.pages) {
-		    	var from = ((this.count+1) - 1) * itemsPerPage + 1;
-			var to = from + itemsPerPage - 1;
-			this.showRecords(from, to);
-		}
-		
-		var element = document.getElementById("pageNavPosition");
-	    	
-	    	var pagerHtml = '<span onclick="' + "pager" + '.prev();" class="pg-normal"> &#171 Prev </span> | ';
-		for (var page = this.count+1; page <= this.count+3; page++) 
-		    pagerHtml += '<span id="pg' + page + '" class="pg-normal" onclick="' + "pager" + '.showPage(' + page + ');">' + page + '</span> | ';
-		pagerHtml += '<span onclick="'+"pager"+'.next();" class="pg-normal"> Next &#187;</span>';            
-		
-		element.innerHTML = pagerHtml;
-        }else{
-        	this.count = 0;
-        }
-    }
-    
-    /*
-    next button to display the next 3 pages and reset the navigator accordigly
-    */
-    this.next = function() {
-    	var endFlag = false;
-    	this.count = this.count + 3;
-        if (this.currentPage < this.pages) {
-            	var from = ((this.count+1) - 1) * itemsPerPage + 1;
-        	var to = from + itemsPerPage - 1;
-        	this.showRecords(from, to);
-        }
-        var element = document.getElementById("pageNavPosition");
-        var pagerHtml = '<span onclick="' + "pager" + '.prev();" class="pg-normal"> &#171 Prev </span> | ';
-        for(var every_page = this.count+1; every_page <= this.count+3; every_page++){
-        	
-        	pagerHtml += '<span id="pg' + every_page + '" class="pg-normal" onclick="' + "pager" + '.showPage('+ every_page + ');">';
-        	if(this.records >= (every_page * 10)-9){
-        		//alert(this.records+" " + every_page + " " + this.records % every_page);
-			pagerHtml += every_page + '</span> | ';
-		}else{
-			endFlag = true;
-		}
-        }
-        if(endFlag == false){
-        	pagerHtml += '<span onclick="'+"pager"+'.next();" class="pg-normal"> Next &#187;</span>';            
-        }
-        element.innerHTML = pagerHtml;
-    }                        
-    
-    /*
-    get the total items from table rows and total pages to be display 
+    get the total no of records and no of pages to be displayed 
     */
     this.init = function() {
         var rows = document.getElementById(tableName).rows;
         this.records = (rows.length);
         this.pages = Math.ceil(this.records / itemsPerPage);
-        this.inited = true;
     }
-
+	
     /*
     show page navigator
     */
-    this.showPageNav = function(pagerName, positionId) {
-    	if (! this.inited) {
-    		alert("not inited");
-    		return;
-    	}
-    	var element = document.getElementById(positionId);
-    	
-    	var pagerHtml = '<span onclick="' + pagerName + '.prev();" class="pg-normal"> &#171 Prev </span> | ';
-        for (var page = 1; page <= 3; page++) 
-            pagerHtml += '<span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showPage(' + page + ');">' + page + '</span> | ';
-        pagerHtml += '<span onclick="'+pagerName+'.next();" class="pg-normal"> Next &#187;</span>';            
+    this.showPageNav = function() {
+        var ul = document.getElementById("unordered-list");
         
-        element.innerHTML = pagerHtml;
+        //add PREV link and set click event
+        var prev = document.createElement('li');
+    	ul.appendChild(prev);
+    	prev.innerHTML="<a class='pg-normal' style='cursor:hand; cursor:pointer;'>Prev</a>";
+    	prev.addEventListener("click", function (){
+    		if (currentPage >= 2){
+            		context.showPage(currentPage - 1);
+            	}else{
+            		//unfocus prev link from here when it comes to the first page
+            	}
+    	} );
+    	
+    	//add pages links and set click event to display list in table row depending
+    	//upon selected page
+    	for (var page = 1; page <= 3; page++) {
+		var page_number = document.createElement('li');
+		page_number.innerHTML="<a id='pg" + page + "' class='pg-normal' onclick='pager.showPage(" + page + ");' style='cursor:hand; cursor:pointer;' >"+page+"</a>";
+    		
+		ul.appendChild(page_number);
+		
+	}
+    	
+    	//add NEXT link and set click event
+    	var next = document.createElement('li');
+    	ul.appendChild(next);
+    	next.innerHTML="<a class='pg-normal' style='cursor:hand; cursor:pointer;'>Next</a>";
+    	next.addEventListener("click", function (){
+    		if (currentPage >= 1 && currentPage+1 <= context.pages){
+            		context.showPage(currentPage + 1);
+            	}else{
+            		//unfocus next link from here when it comes to the last page
+            	}
+    	});
+    	
+    	
+    	
     }
 }
 
