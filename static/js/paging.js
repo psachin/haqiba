27ec561118fbd49c7bac686769ca0c737b600c3f
psachin <iclcoolster@gmail.com>
2013-11-30 20:51:07 +0530

    Subject: Pagination for all list on left-panel.
    Description:

--- static/js/paging.js
+++ static/js/paging.js
@@ -1,23 +1,87 @@
-//js module for pagination
+// JS module for pagination
 
 var context;
-var old_page = 1;
-var next_counter = 3;
+var package_context;
+var bundle_context;
 
-function Pager(tableName, itemsPerPage) {
+function Pager(tableName, itemsPerPage, listID) {
     context = this;
     this.tableName = tableName;
+    this.listID = listID;
     this.itemsPerPage = itemsPerPage;
-    currentPage = 1;
     this.pages = 0;
     this.records;
+    this.next_counter = 3;
+    this.old_page = 1;
+    this.currentPage = 1;
+    
+    //code before paging
+    init(tableName, context);
+    //show page navigator
+    showPageNav(tableName, context, listID, 'c'); 
+    //show page
+    showPage(1, tableName);
+}
+
+function Packages(tableName, itemsPerPage, listID) {
+    package_context = this;
+    this.tableName = tableName;
+    this.listID = listID;
+    this.itemsPerPage = itemsPerPage;
+    this.pages = 0;
+    this.records;
+    this.next_counter = 3;
+    this.old_page = 1;
+    this.currentPage = 1;
+    
+    //code before paging
+    init(tableName, package_context);
+    //show page navigator
+    showPageNav(tableName, package_context, listID, 'p'); 
+    //show page
+    showPage(1, tableName);
+}
+
+function Bundles(tableName, itemsPerPage, listID) {
+    bundle_context = this;
+    this.tableName = tableName;
+    this.listID = listID;
+    this.itemsPerPage = itemsPerPage;
+    this.pages = 0;
+    this.records;
+    this.next_counter = 3;
+    this.old_page = 1;
+    this.currentPage = 1;
+    
+    //code before paging
+    init(tableName, bundle_context);
+    //show page navigator
+    showPageNav(tableName, bundle_context, listID, 'b'); 
+    //show page
+    showPage(1, tableName);
+}
+
+
+/*
+    get the total no of records and no of pages to be displayed 
+    */
+    function init(table, cont) {
+        var rows = document.getElementById(table).rows;
+        cont.records = (rows.length);
+        cont.pages = Math.ceil(cont.records / cont.itemsPerPage);
+    }
     
     /*
     display specific records in a table row 
     */
-    this.showRecords = function(from, to) {        
-        var rows = document.getElementById(tableName).rows;
-        // i starts from 1 to skip table header row
+    function showRecords(from, to, tableName) {   
+    	try{
+    		var rows = document.getElementById(tableName).rows;
+    	}
+    	catch(err){
+    		var rows = document.getElementById(tableName.id).rows;
+    	}   
+    
         for (var i = 0; i < rows.length; i++) {
             if (i < from || i > to)  
                 rows[i].style.display = 'none';
@@ -30,36 +94,52 @@ function Pager(tableName, itemsPerPage) {
     set style for old page anchor and new page anchor
     and get the range of list items to be shown for selected page
     */
-    this.showPage = function(pageNumber) {
-        var oldPageAnchor = document.getElementById('pg'+old_page);
-        oldPageAnchor.style.textDecoration = 'none';
+    function showPage(pageNumber, tableName) {
+    	var cont;
+    	var id_extension;
+    	
+	if(tableName == 'code'){
+		cont = context;
+		id_extension = 'c';
+	}else if(tableName.id == 'code'){
+		cont = context;
+		id_extension = 'c';
+	}else if(tableName == 'package'){
+		cont = package_context;
+		id_extension = 'p';
+	}else if(tableName.id == 'package'){
+		cont = package_context;
+		id_extension = 'p';
+	}else if(tableName == 'bundle'){
+		cont = bundle_context;
+		id_extension = 'b';
+	}else if(tableName.id == 'bundle'){
+		cont = bundle_context;
+		id_extension = 'b';
+	}
+    	   
+        var oldPageAnchor = document.getElementById('pg'+id_extension+cont.old_page);
+        oldPageAnchor.style.textDecoration = 'none';	
         oldPageAnchor.style.fontWeight = "normal";
         
-        currentPage = pageNumber;
-        var newPageAnchor = document.getElementById('pg'+currentPage);
+        cont.currentPage = pageNumber;
+        var newPageAnchor = document.getElementById('pg'+id_extension+cont.currentPage);
         newPageAnchor.style.textDecoration = 'underline';
         newPageAnchor.style.fontWeight = "bold";
         
-        var from = (pageNumber - 1) * itemsPerPage;
-        var to = from + itemsPerPage - 1;
-        this.showRecords(from, to);
-        old_page = currentPage;
+        cont.old_page = cont.currentPage;
+        
+        var from = (pageNumber - 1) * cont.itemsPerPage; //need to add context.itemsperpage
+        var to = from + cont.itemsPerPage - 1; //need to add context.itemsperpage
+        showRecords(from, to, tableName);
+        
     }       
-    
-    /*
-    get the total no of records and no of pages to be displayed 
-    */
-    this.init = function() {
-        var rows = document.getElementById(tableName).rows;
-        this.records = (rows.length);
-        this.pages = Math.ceil(this.records / itemsPerPage);
-    }
 	
     /*
     show page navigator
     */
-    this.showPageNav = function() {
-        var ul = document.getElementById("unordered-list");
+    function showPageNav(tableName, cont, listID, id_extension) {
+        var ul = document.getElementById(listID);
         
         //add PREV link and set click event
         var prev = document.createElement('li');
@@ -68,9 +148,9 @@ function Pager(tableName, itemsPerPage) {
     	prev.addEventListener("click", function (){
     		try
   		{
-	    		if (currentPage > 1){
+	    		if (cont.currentPage > 1){
 	    			//display page which is visible in the navigator
-	    			context.showPage(currentPage - 1);
+	    			showPage((cont.currentPage - 1),tableName);
 		    	}
             	}
 		catch(err)
@@ -79,16 +159,17 @@ function Pager(tableName, itemsPerPage) {
  	 		//visible in the navigator add that page link in the navigator 
  	 		//remove the extreme right page link
  	 		var page_number = document.createElement('li');
-			page_number.innerHTML="<a id='pg" + (currentPage) + "' class='pg-normal' onclick='pager.showPage(" + (currentPage) + ");' style='cursor:hand; cursor:pointer;' >"+(currentPage)+"</a>";
-    			
+ 	 		
+	    		page_number.innerHTML="<a id='pg"+id_extension + (cont.currentPage) + "' class='pg-normal' onclick='showPage("+cont.currentPage+","+tableName+");' style='cursor:hand; cursor:pointer;' >"+(cont.currentPage)+"</a>";
+	    			
     			//remove extreme right link page from navigator
-			$('#pg'+(currentPage+3)).remove();
-			
+			$('#pg'+id_extension+(cont.currentPage+3)).remove();
+		    	
 			//add new link to the left of navigator
-			$('#unordered-list li:eq(1)').after(page_number);
+			$('#'+listID+' li:eq(1)').after(page_number);
 			
 			//show page
-			context.showPage((currentPage));
+			showPage((cont.currentPage), tableName);
 			
  	 		
  	 	}
@@ -97,17 +178,17 @@ function Pager(tableName, itemsPerPage) {
     	//add pages links and set click event to display list in table row depending
     	//upon selected page
     	var count = 3;
-    	if(context.pages == 2 ){
+    	if(cont.pages == 2 ){
     		count = 2;
-    	}else if(context.pages == 1){
+    	}else if(cont.pages == 1){
     		count = 1;
     	}
     	
     	for (var page = 1; page <= count; page++) {
     		
 		var page_number = document.createElement('li');
-		page_number.innerHTML="<a id='pg" + page + "' class='pg-normal' onclick='pager.showPage(" + page + ");' style='cursor:hand; cursor:pointer;' >"+page+"</a>";
-    		
+		
+    		page_number.innerHTML="<a id='pg"+id_extension + page + "' class='pg-normal' onclick='showPage(" +page+","+tableName+ ");' style='cursor:hand; cursor:pointer;' >"+page+"</a>";
 		ul.appendChild(page_number);
 		
 	}
@@ -120,9 +201,9 @@ function Pager(tableName, itemsPerPage) {
     		try
   		{
   			//add next page which is not viewed before
-	    		if (currentPage < context.pages){
+	    		if (cont.currentPage < cont.pages){
 	    			//show page
-				context.showPage(currentPage + 1);
+				showPage((cont.currentPage + 1),tableName);
 		    	}
             	}
 		catch(err)
@@ -131,24 +212,22 @@ function Pager(tableName, itemsPerPage) {
  	 		//visible in the navigator, add that page link in the navigator 
  	 		//remove the extreme left page link
 	    		var page_number = document.createElement('li');
-			page_number.innerHTML="<a id='pg" + (currentPage) + "' class='pg-normal' onclick='pager.showPage(" + (currentPage) + ");' style='cursor:hand; cursor:pointer;' >"+(currentPage)+"</a>";
-    		
+	    		
+	    		
+	    		page_number.innerHTML="<a id='pg"+id_extension + (cont.currentPage) + "' class='pg-normal' onclick='showPage(" +cont.currentPage+","+tableName+");' style='cursor:hand; cursor:pointer;' >"+(cont.currentPage)+"</a>";
+	    		
     			//remove prev link(extreme left)
-			$('#pg'+(currentPage-3)).remove();
-			
+			$('#pg'+id_extension+(cont.currentPage-3)).remove();
+		    	
+		    	
 			//add new link to the right of the navigator
-			$('#unordered-list li:eq('+next_counter+')').after(page_number);
+			$('#'+listID+' li:eq('+cont.next_counter+')').after(page_number);
 			
-			next_counter++;
+			cont.next_counter++;
 			
 			//show page
-			context.showPage(currentPage);
+			showPage(cont.currentPage, tableName);
  	 	}
     		
     	});
-    	
-    	
-    	
     }
-}
-
