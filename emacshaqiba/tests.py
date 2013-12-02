2202abf5746e8550d6c7d23414e881aef4ba9326
psachin <iclcoolster@gmail.com>
2013-12-03 00:18:10 +0530

    Subject: Test cases for codetemplate.
    Description:
    
    - Basic tests for db.
    
    * TODO
    - Test for views using selenium.

--- emacshaqiba/tests.py
+++ emacshaqiba/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from emacshaqiba.models import CodeTemplate
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+        
+class CodeTemplateTests(TestCase):
+    def test_name(self):
+        codetemplate = CodeTemplate(name="shell",
+                                    gist_url="http://gist.github.com/psachin/00000",
+                                    code="(buffer-name)",
+                                    description="test description")
+
+        self.assertEqual(codetemplate.name, "shell")
+        self.assertEqual(codetemplate.gist_url, "http://gist.github.com/psachin/00000")
+        self.assertEqual(codetemplate.code, "(buffer-name)")
+        self.assertEqual(codetemplate.description, "test description")
