583e95a5d0e1c2e97ed397b026419bb2958630f3
Sachin Patil <psachin@redhat.com>
2016-05-09 19:43:09 +0530

    Fixes #1 again!

--- emacshaqiba/human_readable_size.py
+++ emacshaqiba/human_readable_size.py
@@ -1,29 +1,15 @@
 """Convert numbers into human readable form.
 """
 
-from __future__ import division
-
-si = [
-    (1000 ** 5, 'P'),
-    (1000 ** 4, 'Tr'),
-    (1000 ** 3, 'Bn'),
-    (1000 ** 2, 'Mn'),
-    (1000 ** 1, 'K'),
-    (1000 ** 0, ''),
-    ]
-
-
-def hr_size(num, system=si):
+def hr_size(size):
     """Human-readable file size.
     """
-    for factor, suffix in system:
-        #print num, factor
-        if num >= factor:
-            break
-    amount = round(num/factor, 1)
-    # '{0:g}'.format(amount) will discard the decimal places if amount
-    # ends with .0
-    return str('{0:g}'.format(amount)) + suffix
+    suffixes = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    for suffix in suffixes:
+        if size < 1000:
+            return "%s %s" % (size, suffix)
+        size = size // 1000
+    return "%s %s" % (size, suffix)
 
 # print hr_size(5232)
 
